e22e7b3f1ca2b58d6e945c22a3bbfcf2a0c30df4
Firat Saltik <f_saltik@yahoo.com>
2020-02-20 06:47:34 +0300

    Connect api & using forms

--- quotes/views.py
+++ quotes/views.py
@@ -3,7 +3,29 @@ from django.shortcuts import render
 # Create your views here.
 
 def home(request):
-    return render(request,'home.html', {})
+    import requests
+    import json
+
+    if request.method == 'POST':
+        ticker = request.POST['ticker']
+        # pk_6d69e7c204024d179f9b2e76dac0a95f
+        api_request = requests.get(
+            "https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_6d69e7c204024d179f9b2e76dac0a95f")
+        try:
+            api = json.loads(api_request.content)
+
+        except Exception as e:
+            api = "Error..."
+
+        return render(request, 'home.html', {'api': api})
+
+    else:
+        return render(request, 'home.html', {'ticker': "Enter a ticker symbol above..."})
+
+
+
+
+    return render(request,'home.html', {'api': api})
 
 def about(requst):
     return  render(requst,'about.html',{})
