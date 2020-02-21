d5030160b483ab58e0ea2c869336de75083273fd
Firat Saltik <f_saltik@yahoo.com>
2020-02-21 06:51:14 +0300

    Stock page & stock class model & add model to admin & add ticker to databse & django messages & table configuration

--- quotes/urls.py
+++ quotes/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('home', views.home, name='home'),
     path('about', views.about, name='about'),
+    path('add_stock', views.add_stock, name='add_stock'),
+    path('delete/<stock_id>', views.delete, name='delete'),
+
 ]
--- quotes/views.py
+++ quotes/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from .models import Stock
+from .forms import StockForm
+
 
 # Create your views here.
 
@@ -22,10 +26,28 @@ def home(request):
     else:
         return render(request, 'home.html', {'ticker': "Enter a ticker symbol above..."})
 
+    return render(request, 'home.html', {'api': api})
 
-
-
-    return render(request,'home.html', {'api': api})
 
 def about(requst):
-    return  render(requst,'about.html',{})
+    return render(requst, 'about.html', {})
+
+
+def add_stock(request):
+
+    if request.method == 'POST':
+        form = StockForm(request.POST or None)
+        form.save()
+        messages.success(request, ("Stock has been added"))
+        return redirect('add_stock')
+
+    else:
+        ticker = Stock.objects.all()
+        return render(request, 'add_stock.html', {'ticker': ticker})
+
+def delete(request, stock_id):
+    item = Stock.objects.get(pk=stock_id)
+    item.delete()
+    messages.success(request, ("Stock has been deleted"))
+    return redirect(add_stock)
+
