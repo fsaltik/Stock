7065585c10f5b97e834153830bf7bf0be84e783f
Firat Saltik <f_saltik@yahoo.com>
2020-02-23 00:39:11 +0300

    output to table & delete item

--- quotes/views.py
+++ quotes/views.py
@@ -34,6 +34,8 @@ def about(requst):
 
 
 def add_stock(request):
+    import requests
+    import json
 
     if request.method == 'POST':
         form = StockForm(request.POST or None)
@@ -43,11 +45,40 @@ def add_stock(request):
 
     else:
         ticker = Stock.objects.all()
-        return render(request, 'add_stock.html', {'ticker': ticker})
+        output = []
+        for ticker_item in ticker:
+            api_request = requests.get(
+                "https://cloud.iexapis.com/stable/stock/" + str(ticker_item) +
+                "/quote?token=pk_6d69e7c204024d179f9b2e76dac0a95f")
+            try:
+                api = json.loads(api_request.content)
+                output.append(api)
+            except Exception as e:
+                api = "Error..."
+        return render(request, 'add_stock.html', {'ticker': ticker, 'output': output})
 
 def delete(request, stock_id):
     item = Stock.objects.get(pk=stock_id)
     item.delete()
     messages.success(request, ("Stock has been deleted"))
-    return redirect(add_stock)
+    return redirect(delete_stock)
+
+def delete_stock(request):
+    ticker = Stock.objects.all()
+    return render(request, 'delete_stock.html', {'ticker': ticker})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
